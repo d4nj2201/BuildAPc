46e9f892d3eabea465a63d10f9a4841306a604ec
Josh Harrison <P2573342@my365.dmu.ac.uk>
2021-02-15 21:21:55 +0000

    Added all the properties and testing for the Staff management table.

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,9 +1,18 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
         public clsStaff()
         {
         }
+
+        public bool IsWorking { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public double HourlyWage { get; set; }
+        public long PhoneNumber { get; set; }
+        public string FullName { get; set; }
+        public long StaffId { get; set; }
     }
 }
