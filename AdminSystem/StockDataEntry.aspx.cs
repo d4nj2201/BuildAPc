852b4b7732312ca6968ca607f856db03b5a228a9
Daniel Judd <daniel@thejudds.co.uk>
2021-02-21 20:57:58 +0000

    Added basic presentation layer and find method

--- AdminSystem/StockDataEntry.aspx.cs
+++ AdminSystem/StockDataEntry.aspx.cs
@@ -4,11 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsStock
+        clsStock AStock = new clsStock();
+        //capture the StockID
+        AStock.StockID = Convert.ToInt32(txtStockID.Text);
+        //Store the stock ID in the session object
+        Session["AStock"] = AStock;
+        //navigate to the viewer page
+        Response.Redirect("StockViewer.aspx");
     }
 }
