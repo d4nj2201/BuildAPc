44b02b1a79b504b89690dc57246f3c10bbfb3886
Doha Fekouss <P2576498@my365.dmu.ac.uk>
2021-02-24 01:06:43 +0000

    Find method for middle layer

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -4,11 +4,101 @@ namespace ClassLibrary
 {
     public class clsCustomer
     {
-        public bool Active { get; set; }
-        public DateTime DateAdded { get; set; }
-        public short CustomerId { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Address { get; set; }
+        private Int16 mCustomerId;
+        public Int16 CustomerId
+        {
+            get
+            {
+                return mCustomerId;
+
+            }
+            set
+            {
+                mCustomerId = value;
+            }
+        }
+
+        private bool mActive;
+        public bool Active
+        {
+            get
+            {
+                return mActive;
+
+            }
+            set
+            {
+                mActive = value;
+            }
+        }
+
+        private DateTime mDateAdded;
+        public DateTime DateAdded
+        {
+            get
+            {
+                return mDateAdded;
+
+            }
+            set
+            {
+                mDateAdded = value;
+            }
+        }
+
+        private string mUsername;
+        public string Username
+        {
+            get
+            {
+                return mUsername;
+
+            }
+            set
+            {
+                mUsername = value;
+            }
+        }
+
+        private string mPassword;
+        public string Password
+        {
+            get
+            {
+                return mPassword;
+
+            }
+            set
+            {
+                mPassword = value;
+            }
+        }
+
+        private string mAddress;
+        public string Address
+        {
+            get
+            {
+                return mAddress;
+
+            }
+            set
+            {
+                mAddress = value;
+            }
+        }
+
+
+        public bool Find(int customerId)
+        {
+            mCustomerId = 1;
+            mDateAdded = Convert.ToDateTime("20/02/2021");
+            mUsername = "doha";
+            mPassword = "password";
+            mAddress = "36 Treehill Road LE3 7GH";
+            mActive = true;
+            //always return true
+            return true;
+        }
     }
 }
