8875b4bc5c68b48860b94d5c657dcf5aec1bc2ce
Patrick Carroll <P2573147@my365.dmu.ac.uk>
2021-02-24 09:02:27 +0000

    Added some new test cases.

--- Testing4/tstOrders.cs
+++ Testing4/tstOrders.cs
@@ -15,5 +15,31 @@ namespace TestingOrders
             //test to see that it exists
             Assert.IsNotNull(AnOrder);
         }
+
+        [TestMethod]
+        public void PayedPropertyOK()
+        {
+            //create an instance of the class we wnat to create.
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property.
+            Boolean TestData = true;
+            //assing the data to the property.
+            AnOrder.Active = TestData;
+            //test to see that the two values are the same.
+            Assert.AreEqual(AnOrder.Active, TestData);
+        }
+
+        [TestMethod]
+        public void DeliveryTimePropertyOK()
+        {
+            //create an instance of the class we want to create.
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property.
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property.
+            AnOrder.DateAdded = TestData;
+            //test to see that the two values are equal.
+            Assert.AreEqual(AnOrder.DateAdded, TestData);
+        }
     }
 }
