d9924a791d50e7ad8ad10774ae2926cbddf0ce58
Doha Fekouss <P2576498@my365.dmu.ac.uk>
2021-05-18 02:53:00 +0100

    updating work

--- Testing2/tstCustomerCollection.cs
+++ Testing2/tstCustomerCollection.cs
@@ -43,18 +43,6 @@ namespace TestingCustomer
             Assert.AreEqual(AllCustomers.CustomerList, TestList);
            
         }
-        [TestMethod]
-        public void CountPropertyOK()
-        {
-            //create an instance of the class we want to create
-            clsCustomerCollection AllCustomers = new clsCustomerCollection();
-            //create some test data to assign to the property
-            Int32 SomeCount = 2;
-            //assign the data to the property
-            AllCustomers.Count = SomeCount;
-            //test to see that the two values are the same
-            Assert.AreEqual(AllCustomers.Count, SomeCount);
-        }
 
         [TestMethod]
         public void ThisCustomerPropertyOK()
@@ -102,15 +90,6 @@ namespace TestingCustomer
             AllCustomers.CustomerList = TestList;
             //test to see that the two values are the same
             Assert.AreEqual(AllCustomers.Count, TestList.Count);
-
-        }
-        [TestMethod]
-        public void TwoRecordsPresent()
-        {
-            //create an instance of the class we want to create
-            clsCustomerCollection AllCustomers = new clsCustomerCollection();
-            //test to see that the two values are the same
-            Assert.AreEqual(AllCustomers.Count, 2);
 
         }
     }
